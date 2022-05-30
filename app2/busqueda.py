f14d854faef835362f7412ef39c25993c53cf111
EstebanAlarconO <esteban.alarcon1@mail.udp.cl>
2022-05-30 00:49:03 -0400

    Ultimos cambios

--- app2/busqueda.py
+++ app2/busqueda.py
@@ -8,6 +8,7 @@ busquedas = ('Trap Card', 'Normal Monster', 'Flip Effect Monster', 'Effect Monst
 'Ritual Effect Monster', 'Gemini Monster', 'Toon Monster', 'Pendulum Normal Monster', 'Pendulum Flip Effect Monster', 'Synchro Tuner Monster', 
 'XYZ Pendulum Effect Monster', 'Pendulum Tuner Effect Monster', 'Synchro Pendulum Effect Monster')
 
+
 ELASTIC_PASSWORD = "EjZC5k4FiUu70FvUYXUHkI1M"
 CLOUD_ID = "Tutorial:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJDcwOGUyODI1ODUzODQzNmI5ZjRlMjI1ZGE2MTIxMTQ1JDFhNTA5MjA0NzA5MzRjNDViOGQ5ZmY2ZDMyNTVlZGMz"
 
@@ -21,6 +22,19 @@ if __name__== "__main__":
 
     while True:
         carta = random.choice(busquedas)
-        resp = con.search(index="cards", query={"match_all": {}})
-        print(resp)
-        sleep(5)
+        query_body = {"size": 10000,
+                        "query": {
+                            "bool": {
+                            "must": [
+                                {"term": {
+                                "Type.keyword": carta
+                                }}
+                            ]
+                            }
+                        }
+                    }
+
+        resp = con.search(index="cards", query=query_body)
+        valor = [doc['_source'] for doc in resp['hits']['hits']]
+        print(valor[:10])
+        sleep(10)
